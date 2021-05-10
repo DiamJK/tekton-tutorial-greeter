ec53cf18e138872d4ee5580113ffba651ed31f38
DiamJK <83073406+DiamJK@users.noreply.github.com>
2021-05-10 11:08:53 +0200

    Update GreetingResource.java

--- quarkus/src/main/java/com/redhat/developers/GreetingResource.java
+++ quarkus/src/main/java/com/redhat/developers/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String hello() {
-    return "Meeow!! from Tekton 😺🚀";
+    return "Meeow!! Julia from Tekton 😺🚀";
   }
 }
